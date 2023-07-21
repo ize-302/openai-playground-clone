5e566e65c38e8a12b11a5b1c2163a38fe4de8d2a
ize-send <adavize.hassan@send.ng>
2023-07-21 08:33:57 +0100

    first commit

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+// Create an OpenAI API client
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+// Set the runtime to edge for best performance
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  let { messages } = await req.json();
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: messages,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+// ./app/page.js
+"use client";
 
-export default function Home() {
+import React from "react";
+import { useChat } from "ai/react";
+import { Toaster, toast } from "sonner";
+import { IconRobot, IconUser, IconSend, IconCopy } from "@tabler/icons-react";
+
+export default function Chat() {
+  const [message, setmessage] = React.useState("");
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    body: {
+      message,
+    },
+  });
+  const [savedPlayground, setsavedPlayground] = React.useState();
+
+  const onSubmit = (e: any) => {
+    handleSubmit(e);
+  };
+
+  const lastMessage = messages[messages.length - 1];
+  const detectedTone =
+    lastMessage?.role === "assistant" ? lastMessage.content : null;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between max-w-4xl w-full mx-auto border-l border-r">
+        <div className="relative flex flex-col gap-2 w-full z-[-1] overflow-y-auto">
+          <p className="text-xl font-semibold p-3 border-b">Playground</p>
+          {messages.map((m) => (
+            <div key={m.id}>
+              <div className="px-4 py-1 text-sm flex gap-2">
+                <div>
+                  <div
+                    className={`w-6 h-6 grid place-content-center ${
+                      m.role === "user" ? "bg-black" : "bg-green-400"
+                    } ${
+                      m.role === "user" ? "text-white" : "text-black"
+                    } rounded-md`}
+                  >
+                    {m.role === "user" ? (
+                      <IconUser size={16} />
+                    ) : (
+                      <IconRobot size={16} />
+                    )}
+                  </div>
+                </div>
+                {m.content}{" "}
+                {m.role !== "user" && (
+                  <div
+                    className="cursor-copy"
+                    onClick={() => {
+                      navigator.clipboard.writeText(m.content);
+                      toast("copied!");
+                    }}
+                  >
+                    <IconCopy
+                      size={20}
+                      className="text-gray-600"
+                      stroke={1.4}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="border-t mx-auto w-full max-w-lg" />
+            </div>
+          ))}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="mb-0 w-full ">
+          <form
+            onSubmit={onSubmit}
+            className="w-full flex items-end border-t gap-2 sticky bottom-0 p-3"
+          >
+            <input
+              className="w-full rounded-md border border-gray-800 py-2 px-4 shadow-sm focus:border-black focus:ring-black"
+              value={input}
+              placeholder="write prompt here"
+              onChange={(e) => {
+                handleInputChange(e);
+                setmessage(e.target.value);
+              }}
+            />
+            <button
+              type="submit"
+              disabled={!input}
+              className={`bg-green-400 text-black-700 whitespace-nowrap h-10 px-3 py-2 text-sm rounded-md ${
+                !input ? "opacity-50" : "opacity-100"
+              }`}
+            >
+              <IconSend />
+            </button>
+          </form>
+        </div>
+      </main>
+      <Toaster position="bottom-center" />
+    </>
+  );
 }
